77199dd298a4e65e2c11ae3f4a9c32be5f7f4747
kireeti <kireeti@accendo.com.my>
2020-04-03 13:40:21 +0800

    frontend react stepup with typescript and scss is done

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import style from './app.module.scss';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
